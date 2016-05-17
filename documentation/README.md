24dec586bb93d466355b92079d06fb720e80cc64
BenLatham <benlatham123@hotmail.co.uk>
2016-05-17 20:27:02 +0100

    here arethose missing directories!

---
+++ documentation/README.md
@@ -0,0 +1,13 @@
+# Project-WebTech
+
+Our title is : ScienceCollective
+
+Our purpose : Working together to review the most relevant scientific discoveries every week, sharing the images and videos which most inform and engage us. We aim to make science more accessible.
+
+Note on content:
+
+As a rule images in this project are used only to demonstrate the function of the site in liew of future content which would be added prior to release if the site were released live; This is due to the fact that this is a computer science project, and we wish to focus on developing functionality, testing, etc. As such we do not claim to own the copyrights on all images currently included, or to have created them, however we have attempted to use images which are licenced to share using creative-commons licences or are in the public domain. Image sources are listed in the sources.txt file.
+
+We have however included images which we have created:
+These include NeuronWorld which was created using gimp and inkscape.
+
---
+++ resources/scripts/script.js
@@ -0,0 +1,77 @@
+"use strict";
+addEventListener('load', start);
+
+function start() {
+  var count = 0;
+  var mainPhoto = document.querySelector('#mainphoto');
+  var leftArrow = document.querySelector('#left');
+  var rightArrow = document.querySelector('#right');
+        
+  var dot0 = document.querySelector('#dot0');
+  var dot1 = document.querySelector('#dot1');
+  var dot2 = document.querySelector('#dot2');
+  var dot3 = document.querySelector('#dot3');
+  var dot4 = document.querySelector('#dot4');
+
+  dot0.onclick = function() {
+    count=0;
+    pick();
+  }
+  dot1.onclick = function() {
+    count=1;
+    pick();
+  }
+  dot2.onclick = function() {
+    count=2;
+    pick();
+  }
+  dot3.onclick = function() {
+    count=3;
+    pick();
+  }
+  dot4.onclick = function() {
+    count=4;
+    pick();
+  }
+
+  rightArrow.onclick = function() {
+    count = (count+1)%5;
+    pick();
+  }
+
+  leftArrow.onclick = function() {
+    count = (count+4)%5;
+    pick();
+  }
+
+  var pick = function(){        
+    dot0.className = "";      
+    dot1.className = "";      
+    dot2.className = "";      
+    dot3.className = "";      
+    dot4.className = "";
+    switch(count){
+      case 0:
+        mainPhoto.setAttribute ('src','images/ant_nebula.jpg');
+        dot0.className = "active";
+        break;
+      case 1:
+        mainPhoto.setAttribute ('src','images/alpheus_bellulus.jpg');
+        dot1.className = "active";
+        break;
+      case 2:
+        mainPhoto.setAttribute ('src','images/bee.jpg');
+        dot2.className = "active";
+        break;
+      case 3:
+        mainPhoto.setAttribute ('src','images/c_auratus_with_prey.jpg');
+        dot3.className = "active";
+        break;
+      case 4:
+        mainPhoto.setAttribute ('src','images/celular.jpg');
+        dot4.className = "active";
+        break;
+    }
+  }
+} 
+
